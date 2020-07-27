aa55549dacf205a6a96ed2409590c9261d9182e0
JuanYut <juanyut97@hotmail.com>
2020-07-27 17:17:59 -0500

    my initial commit :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Logo from "./images/ecommerce.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="title">
+        <img className="logo" src={Logo} alt="logo" />
+        <h1 className="title-text">Products Ecommerce</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">Form:</div>
+          <div className="one-half column">Tabla:</div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
