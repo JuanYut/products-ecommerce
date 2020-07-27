9e8181df2e56ea2bb47836fe2475b5598906d4cb
JuanYut <juanyut97@hotmail.com>
2020-07-27 18:47:36 -0500

    item component with axios and the client API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Logo from "./images/ecommerce.png";
+import Item from "./components/Item";
 
 function App() {
+  const [orders, setOrders] = useState([]);
+
+  let token =
+    "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJkM2NIVUVibVJoc1EzeXhNbzV2VnliSTFzaDZCSDJZRCIsImlhdCI6MTU4NTkzMjYzNDU0OH0.tMSht_M3ryQl5IqCirhYR1gb8j3FQ26vILT4Qpx4XrdFz-zUmqbgFYiKTaZHPpB85etRIMhxVoZf6tOrHy0fnA";
+
+  const getOrders = async () => {
+    let res = await axios.get(
+      "https://eshop-deve.herokuapp.com/api/v2/orders",
+      { headers: { Authorization: token } }
+    );
+    let data = await res.data.orders;
+    setOrders(data);
+  };
+
+  useEffect(() => {
+    getOrders();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <React.Fragment>
       <div className="title">
@@ -12,7 +33,18 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="one-half column">Form:</div>
-          <div className="one-half column">Tabla:</div>
+          <div className="one-half column">
+            <h2>Orders</h2>
+            <div className="order-title">
+              <p>Sku</p>
+              <p>Name</p>
+              <p>Quantity</p>
+              <p>Price</p>
+            </div>
+            {orders.map((order, key) => (
+              <Item order={order} key={key} />
+            ))}
+          </div>
         </div>
       </div>
     </React.Fragment>
